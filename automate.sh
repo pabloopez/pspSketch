254830b635ea4c8fecbd5c4621b203a6e98704ab
pabloopez <pablo.lopezzaldivar@sysdig.com>
2020-01-13 13:11:45 +0100

    another way, apiserver not running with last method

--- automate.sh
+++ automate.sh
@@ -14,8 +14,13 @@ kubectl get pods -A
 mkdir /etc/kubernetes/policies
 mv /root/pspSketch/audit-policy.yaml /etc/kubernetes/policies/audit-policy.yaml
 
-rm /etc/kubernetes/manifests/kube-apiserver.yaml
-mv /root/pspSketch/kube-apiserver.yaml /etc/kubernetes/manifests/kube-apiserver.yaml
+#rm /etc/kubernetes/manifests/kube-apiserver.yaml
+#mv /root/pspSketch/kube-apiserver.yaml /etc/kubernetes/manifests/kube-apiserver.yaml
+
+#sustituye abc por XYZ
+sed -i -e 's/    image:/    - --audit-policy-file=/etc/kubernetes/policies/audit-policy.yaml\n    - --audit-log-path=-\n    - --audit-dynamic-configuration=true\n    - --feature-gates=DynamicAuditing=true\n    - --runtime-config=auditregistration.k8s.io/v1alpha1=true\n    image:/g' /etc/kubernetes/manifests/kube-apiserver.yaml
+sed -i -e 's/  hostNetwork:/    - mountPath: /etc/kubernetes/policies\n      name: policies\n      readOnly: true\n  hostNetwork:/g' /etc/kubernetes/manifests/kube-apiserver.yaml
+sed -i -e 's/status:/  - hostPath:\n      path: /etc/kubernetes/policies\n      type: DirectoryOrCreate\n    name: policies\nstatus:/g' /etc/kubernetes/manifests/kube-apiserver.yaml
 
 #cluster should restart automatically, check with
 kubectl get pods -A
@@ -25,7 +30,7 @@ helm version
 helm repo add stable https://kubernetes-charts.storage.googleapis.com
 helm repo update
 kubectl create ns sysdig-agent
-helm install pablotest \
+helm install training \
     --set sysdig.accessKey=d5ef4566-d0c2-4174-92eb-0727fc0991f3 \
     --set sysdig.settings.tags="role:training\,location:universe" \
     --set auditLog.dynamicBackend.enabled=true \
