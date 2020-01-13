6701341cfd15b07b68366ad81e500975bf64e3ba
pabloopez <pablo.lopezzaldivar@sysdig.com>
2020-01-13 19:23:39 +0100

    remove option kube-apiserver

--- automate.sh
+++ automate.sh
@@ -19,7 +19,8 @@ mv /root/pspSketch/audit-policy.yaml /etc/kubernetes/policies/audit-policy.yaml
 
 #sustituye abc por XYZ
 #sed -i -e 's/abc/XYZ/g' ./path/to.file
-sed -i -e 's/    image:/    - --audit-policy-file=\/etc\/kubernetes\/policies\/audit-policy.yaml\n    - --audit-log-path=-\n    - --audit-dynamic-configuration=true\n    - --feature-gates=DynamicAuditing=true\n    - --runtime-config=auditregistration.k8s.io\/v1alpha1=true\n    image:/g' /etc/kubernetes/manifests/kube-apiserver.yaml
+#sed -i -e 's/    image:/    - --audit-policy-file=\/etc\/kubernetes\/policies\/audit-policy.yaml\n    - --audit-log-path=-\n    - --audit-dynamic-configuration=true\n    - --feature-gates=DynamicAuditing=true\n    - --runtime-config=auditregistration.k8s.io\/v1alpha1=true\n    image:/g' /etc/kubernetes/manifests/kube-apiserver.yaml
+sed -i -e 's/    image:/    - --audit-policy-file=\/etc\/kubernetes\/policies\/audit-policy.yaml\n    - --audit-log-path=-\n    - --audit-dynamic-configuration=true\n    - --feature-gates=DynamicAuditing=true\n    image:/g' /etc/kubernetes/manifests/kube-apiserver.yaml
 sed -i -e 's/  hostNetwork:/    - mountPath: \/etc\/kubernetes\/policies\n      name: policies\n      readOnly: true\n  hostNetwork:/g' /etc/kubernetes/manifests/kube-apiserver.yaml
 sed -i -e 's/status:/  - hostPath:\n      path: \/etc\/kubernetes\/policies\n      type: DirectoryOrCreate\n    name: policies\nstatus:/g' /etc/kubernetes/manifests/kube-apiserver.yaml
 
@@ -33,6 +34,7 @@ helm repo update
 kubectl create ns sysdig-agent
 helm install training \
     --set sysdig.accessKey=d5ef4566-d0c2-4174-92eb-0727fc0991f3 \
+    --set secure.enabled=true \
     --set sysdig.settings.tags="role:training\,location:universe" \
     --set auditLog.dynamicBackend.enabled=true \
     --set auditLog.enabled=true \
